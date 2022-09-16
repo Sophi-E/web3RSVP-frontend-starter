a38998975e9042d3e094fe590c943a2af5a555c0
sophi-e <sophierithous@gmail.com>
2022-09-16 11:17:12 +0100

    rsvp and display events

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/sophi-e/web3-rsvp",
+  uri: "https://api.thegraph.com/subgraphs/name/sophi-e/rsvp",
   cache: new InMemoryCache(),
 });
 
